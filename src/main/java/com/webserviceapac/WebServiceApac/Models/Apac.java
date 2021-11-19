2eb36e39ad9bd77d15d92dd203e6461abaa80c2d
CristianoFCarvalho <(chrisfcarvalho@hotmail.com)>
2021-11-19 01:31:00 -0300

    Atualização!

--- src/main/java/com/webserviceapac/WebServiceApac/Models/Apac.java
+++ src/main/java/com/webserviceapac/WebServiceApac/Models/Apac.java
@@ -204,10 +204,235 @@ public class Apac<DateTime> {
     @NotBlank
     private Date DatDiagnosticoCitoHistopato;
 
+                    //Tratamentos Anteriores
+    @NotBlank
+    private String CodCid;
+
+    @NotBlank
+    private Date DataInicioTratamentoAnterior;
+
+                    //Quimioterapia
+    @NotBlank
+    private Boolean IndContinuidadeTratamento;
+
+    @NotBlank
+    private Date dataInicioTratamento;
+
+    @NotBlank
+    private String  DscEsquema;
+
+    @NotBlank
+    private Long NumTotalMesPlanejado;
+
+            //Radioterapia
+
+    @NotBlank
+   // private Boolean IndContinuidadeTratamento;
+
+    @NotBlank
+    private Date InicioTratamento;
+
+    @NotBlank
+    private EnumTipFinalidadeTratamento TipFinalidadeTratamento;
+
+    @NotBlank
+  //  private String CodCid;
+
+            //Area Irradiada
+    @NotBlank
+    private String DscAreaIrradiada;
+
+    @NotBlank
+    private Long Numincercoes;
+
+    @NotBlank
+    private Date DateInicioAreaIrradiada;
+
+    @NotBlank
+    private Date DateTerminoAreaIrradiada;
+
+
+                //Nefrologia
+    @NotBlank
+    private Date DataPrimeiraDialiseRealizada;
+
+    @NotBlank
+    private String VlrPeso;
+
+    @NotBlank
+    private String VlrAltura;
+
+    @NotBlank
+    private String VlrDiurese;
+
+    @NotBlank
+    private String VlrGlicose;
+
+    @NotBlank
+    private String VlrAlbimina;
+
+    @NotBlank
+    private String VlrHbPrimeroAtendimento;
+
+    @NotBlank
+    private Boolean IndAcessoVascular;
+
+    @NotBlank
+    private Boolean IndHivPrimeiroAtendimento;
+
+    @NotBlank
+    private Boolean IndHcvPrimeiroAtendimento;
+
+    @NotBlank
+    private Boolean IndHbsagPrimAtendimento;
+
+    @NotBlank
+    private Boolean IndUltrasonografiaAbdominal;
+
+    @NotBlank
+    private String VlrHbSeguimento;
+
+    @NotBlank
+    private String VlrAlbuminaSeguimento ;
+
+    @NotBlank
+    private Long VlrIntervencaoFistola;
+
+    @NotBlank
+    private Boolean IndListaCncdo;
+
+    @NotBlank
+    private Boolean IndHivSeguimento;
+
+    @NotBlank
+    private Boolean IndHcvSeguimento;
+
+    @NotBlank
+    private Boolean IndHbsagSeguimento;
+
+    @NotBlank
+  //  private Long NumSessoes1Mes;
+
+  // private Long NumSessoes2Mes;
+
+  //  private Long NumSessoes3Mes;
 
 
 
+            //Protese Mamaria
+    @NotBlank
+    private Long CodCnesFces;
 
+    @NotBlank
+    private EnumTipMarcaProtese  TipMarcaProtese;
+
+    @NotBlank
+    private Long VlrAnoImplantacaoProtese;
+
+            //Pre-Bariatrica
+
+    @NotBlank
+    private Date DatPrimeiraAvaliacao;
+
+    @NotBlank
+    private String VlrImcPrimAvaliacao;
+
+    @NotBlank
+ //   private String VlrPeso;
+
+    @NotBlank
+    private Date DatAvaliacaoAtual;
+
+    @NotBlank
+    private String  ValorImcAtual;
+
+    @NotBlank
+    private EnumTipPontuacaoBaros TipPontuacaoBaros;
+
+    @NotBlank
+    private Boolean IndComorbUsoMedicamento;
+
+    @NotBlank
+    private Boolean IndComorbAtividadeFisica;
+
+    @NotBlank
+    private Boolean IndImcPerdaPonderal;
+
+    @NotBlank
+    private Boolean IndParticipaReuniaoGrupo;
+
+    @NotBlank
+    private Boolean IndAvaliacaoRiscosCirurgico;
+
+    @NotBlank
+    private EnumTipoOcupacaoAvaliacao TipoOcupacaoAvaliacao;
+
+    @NotBlank
+    private Boolean IndRealizadoExamesLaboratr;
+
+    @NotBlank
+    private EnumIndAptoProcedCirurgico IndAptoProcedCirurgico;
+
+    @NotBlank
+    private String ListaExamesPreBariatrica;
+
+    @NotBlank
+    private String ListaCidComorbiPreBariatrica;
+
+            //Pos-Bariatrica
+    @NotBlank
+    private String VlrImcAtual;
+
+    @NotBlank
+    private Long VlrPercPesoPerdido;
+
+    @NotBlank
+    private String VlrKgPerdidos;
+
+    @NotBlank
+//    private String CodTabelaSusOficial;
+
+    @NotBlank
+    private Date DatCirurgiaBariatrica;
+
+    @NotBlank
+    private Long NumApacCirurgiaBariatrica;
+
+    @NotBlank
+    private Long NumMesAcompanhamento;
+
+    @NotBlank
+    private Long NumAnoAcompanhamento;
+
+    @NotBlank
+ //   private EnumTipPontuacaoBaros  TipPontuacaoBaros;
+
+    @NotBlank
+    private Boolean IndComorbUsoMedicamentos;
+
+    @NotBlank
+    private Boolean IndComorbusoPolivitaminicos;
+
+    @NotBlank
+//    private Boolean IndComorbAtividadeFisica;
+
+    @NotBlank
+    private Boolean IndiComorbGanhoPeso;
+
+    @NotBlank
+    private Boolean IndComorbAliemtoSaudavel;
+
+                //Plastica Pos-Bariátrica
+    @NotBlank
+ //   private String CodTabelaSusOficial;
+
+    @NotBlank
+    private Long ValosTempoPosBariatrica;
+
+
+                //CID10 Pos-Bariátrica
+    @NotBlank
+    private String  ListaCidComorbidadesPosBariatricas;
 
 
 
---
+++ src/main/java/com/webserviceapac/WebServiceApac/Models/EnumTipFinalidadeTratamento.java
@@ -0,0 +1,11 @@
+package com.webserviceapac.WebServiceApac.Models;
+
+public enum EnumTipFinalidadeTratamento {
+
+    RADICAL,
+    ADJUVANT,
+    ANTALGICA,
+    PALIATIVA,
+    PREVIA,
+    ANTIHEMORRAGICA,
+}
